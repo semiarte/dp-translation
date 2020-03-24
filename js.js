bfae7657329d356f44e41fe45d3db452e1a32d81
semiarte <elsemi6@gmail.com>
2020-03-24 12:05:02 +0100

    css > animación top to bottom de la sección de apertura

--- js.js
+++ js.js
@@ -1,34 +1,34 @@
 // smooth window load
-function loadPage(){
-    loadingTime = setTimeout(showPage, 400);
+function loadPage() {
+  loadingTime = setTimeout(showPage, 400);
 }
-function showPage(){
-    $('.loader').fadeOut();
+function showPage() {
+  $(".loader").fadeOut();
 }
 
 // open hidden nav menu
 function open_menu() {
-    $('.menu-button').hide();
-    $('.menu-button_close').show();
-    $('.nav_menu').fadeIn();
+  $(".menu-button").hide();
+  $(".menu-button_close").show();
+  $(".nav_menu").fadeIn();
 }
 // close hidden nav menu
 function close_menu() {
-    $('.menu-button').show();
-    $('.menu-button_close').hide();
-    $('.nav_menu').fadeOut();
+  $(".menu-button").show();
+  $(".menu-button_close").hide();
+  $(".nav_menu").fadeOut();
 }
 
 // initialize swiper
-var swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    centeredSlides: true,
-    mousewheel: true,
-    forceToAxis: true,
-    setWrapperSize: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        type: 'bullets',
-    },
+var swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  centeredSlides: true,
+  mousewheel: true,
+  forceToAxis: true,
+  setWrapperSize: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets"
+  }
 });
