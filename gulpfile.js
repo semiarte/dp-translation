e448bec692ca8b56f820e19c764ae5ef1cdcb023
Jose Miguel Perez <jmperez@esferize.com>
2020-05-27 11:10:51 +0200

    gulp cssTask

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 // dependencies
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const replace = require('gulp-replace');
 
 const files = {
+    cssPath: 'scss/*.css',
     jsPath:'js.js',
 }
 
 // Tasks
+function cssTask() {
+    return src(files.cssPath)
+        .pipe(postcss([ autoprefixer(), cssnano() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('css'));
+}
+
 function jsTask() {
     return src(files.jsPath)
         .pipe(concat('all.js'))
@@ -26,14 +38,14 @@ function cacheBustTask(){
 // watch
 function watchTask() {
     watch(
-        [files.jsPath],
-        parallel(jsTask)    
+        [files.cssPath, files.jsPath],
+        parallel(cssTask, jsTask)    
     );
 }
 
 // default task
 exports.default = series(
-    parallel(jsTask),
+    parallel(cssTask, jsTask),
     cacheBustTask,
     watchTask
 );
